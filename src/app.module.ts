f1d78ad6ca81dee6e65fc4e91cc05ba475d8efe1
mohadese <mohiw.1380@gmail.com>
2025-04-27 17:45:21 +0330

    feat
    create task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsModule } from './projects/projects.module';
 import { DatabaseModule } from './config/database.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     DatabaseModule ,
-    ProjectsModule ,
+    ProjectsModule,
+    TasksModule ,
   ],
   controllers: [],
   providers: [],
