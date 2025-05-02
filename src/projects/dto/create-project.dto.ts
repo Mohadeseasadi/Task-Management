dcee42b2dd717c445d609fe228c79913cc4c2e05
mohadese <mohiw.1380@gmail.com>
2025-05-02 18:21:03 +0330

    feat
    added validator to peoject module

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,7 +1,11 @@
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
 import ProjectStatusEnum from "../enums/project-status.enum";
 
 export class CreateProjectDto {
+    @IsNotEmpty({message: "name required"})
+    @IsString()
     name: string ;
     
+    @IsEnum(ProjectStatusEnum , {message: "inavlid status"})
     status: ProjectStatusEnum
 }
